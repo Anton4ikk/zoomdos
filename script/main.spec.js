85ac89b28ab1e22a1d3a297449ad714497094d69
TonyB <66313274+Anton4ikk@users.noreply.github.com>
2024-09-21 12:55:14 +0300

    Dev (#2)
    * update2
    
    * update2
    
    ---------
    
    Co-authored-by: Anton4ikk <anton@MacBook-Pro-Anton.local>

--- script/main.spec.js
+++ script/main.spec.js
@@ -3,29 +3,49 @@ const { users, zoomLink, zoomDuration } = require('../config/index');
 
 for (let user of users) {
   test(`Enter zoom meeting by ${user}`, async () => {
-    const browser = await chromium.launch({
-      args: [
-        '--mute-audio',
-      ]
-    });
+    const browser = await chromium.launch({args: ['--mute-audio',]});
     const browserContext = await browser.newContext();
     let page = await browserContext.newPage();
     await page.goto(zoomLink, { waitUntil: "commit" });
-    await page.waitForTimeout(2000);
+
+    while(!(await page.getByRole('button', { name: 'Accept Cookies' }).isVisible())) {
+      await page.waitForTimeout(2000);
+    }
     await page.getByRole('button', { name: 'Accept Cookies' }).click();
-    await page.waitForTimeout(16 * 1000);
+    
+    while(!(await page.getByRole('button', { name: 'Launch Meeting' }).isVisible())) {
+      await page.waitForTimeout(2000);
+    }
     await page.getByRole('button', { name: 'Launch Meeting' }).click();
     await page.waitForTimeout(2000);
+    
+    while(!(await page.getByRole('button', { name: 'Join from your browser' }).isVisible())) {
+      await page.waitForTimeout(2000);
+    }
     await page.getByRole('button', { name: 'Join from your browser' }).click();
-    await page.waitForTimeout(Math.floor(Math.random() * 180) * 1000);
+
+    // await page.waitForTimeout(Math.floor(Math.random() * 180) * 1000);
+    await page.waitForTimeout(Math.floor(Math.random() * 20) * 1000);
     page = browserContext.pages()[0];
     await page.goto(page.url(), { waitUntil: "commit" });
-    await page.waitForTimeout(8 * 1000);
+
+    while(!(await page.frameLocator('#webclient').getByLabel('Your Name').isVisible())) {
+      await page.waitForTimeout(2000);
+    }
     await page.frameLocator('#webclient').getByLabel('Your Name').fill(user);
-    await page.waitForTimeout(2000);
+
+    while(!(await page.frameLocator('#webclient').getByRole('button', { name: 'Join', exact: true }).isVisible())) {
+      await page.waitForTimeout(2000);
+    }
     await page.frameLocator('#webclient').getByRole('button', { name: 'Join', exact: true }).click();
-    await page.waitForTimeout(16 * 1000);
+
+    while(!(await page.frameLocator('#webclient').getByRole('button', { name: 'Join Audio by Computer'}).isVisible())) {
+      await page.waitForTimeout(20 * 1000);
+    }
     await page.frameLocator('#webclient').getByRole('button', { name: 'Join Audio by Computer' }).click();
-    await page.waitForTimeout(zoomDuration * 1.2 * 60 * 1000);
+
+    while (true) {
+      await page.waitForTimeout(10 * 60 * 1000);
+    }
   });
 }
